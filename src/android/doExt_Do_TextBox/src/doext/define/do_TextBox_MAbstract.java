f03fccafa3b7bdafe7f599499f4023f19c8c1f3c
maoruiily <maoruiily@163.com>
2015-04-16 20:15:55 +0800

    update TextBox

--- src/android/doExt_Do_TextBox/src/doext/define/do_TextBox_MAbstract.java
+++ src/android/doExt_Do_TextBox/src/doext/define/do_TextBox_MAbstract.java
@@ -1,4 +1,4 @@
-package extdefine;
+package doext.define;
 
 import core.object.DoUIModule;
 import core.object.DoProperty;
@@ -18,11 +18,11 @@ public abstract class do_TextBox_MAbstract extends DoUIModule{
 	public void onInit() throws Exception{
         super.onInit();
         //注册属性
-		this.registProperty(new DoProperty("text", PropertyDataType.String, "", false));
-		this.registProperty(new DoProperty("fontColor", PropertyDataType.String, "000000", false));
+		this.registProperty(new DoProperty("fontColor", PropertyDataType.String, "000000FF", false));
 		this.registProperty(new DoProperty("fontSize", PropertyDataType.Number, "9", false));
 		this.registProperty(new DoProperty("fontStyle", PropertyDataType.String, "normal", false));
 		this.registProperty(new DoProperty("hint", PropertyDataType.String, "", true));
-		this.registProperty(new DoProperty("maxLength", PropertyDataType.Number, "0", true));
+		this.registProperty(new DoProperty("maxLength", PropertyDataType.Number, "100", true));
+		this.registProperty(new DoProperty("text", PropertyDataType.String, "", false));
 	}
 }
--- src/android/doExt_Do_TextBox/src/doext/implement/do_TextBox_Model.java
+++ src/android/doExt_Do_TextBox/src/doext/implement/do_TextBox_Model.java
@@ -1,6 +1,6 @@
-package extimplement;
+package doext.implement;
 
-import extdefine.do_TextBox_MAbstract;
+import doext.define.do_TextBox_MAbstract;
 
 /**
  * 自定义扩展组件Model实现，继承Do_TextBox_MAbstract抽象类；
