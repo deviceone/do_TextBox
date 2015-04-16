89a7da80fdca49ca9ba4ba11779110bb80369884
maoruiily <maoruiily@163.com>
2015-04-16 21:18:35 +0800

    update

---
+++ src/android/doExt_do_TextBox/src/doext/define/do_TextBox_MAbstract.java
@@ -0,0 +1,28 @@
+package doext.define;
+
+import core.object.DoUIModule;
+import core.object.DoProperty;
+import core.object.DoProperty.PropertyDataType;
+
+
+public abstract class do_TextBox_MAbstract extends DoUIModule{
+
+	protected do_TextBox_MAbstract() throws Exception {
+		super();
+	}
+	
+	/**
+	 * 初始化
+	 */
+	@Override
+	public void onInit() throws Exception{
+        super.onInit();
+        //注册属性
+		this.registProperty(new DoProperty("fontColor", PropertyDataType.String, "000000FF", false));
+		this.registProperty(new DoProperty("fontSize", PropertyDataType.Number, "9", false));
+		this.registProperty(new DoProperty("fontStyle", PropertyDataType.String, "normal", false));
+		this.registProperty(new DoProperty("hint", PropertyDataType.String, "", true));
+		this.registProperty(new DoProperty("maxLength", PropertyDataType.Number, "100", true));
+		this.registProperty(new DoProperty("text", PropertyDataType.String, "", false));
+	}
+}
---
+++ src/android/doExt_do_TextBox/src/doext/implement/do_TextBox_View.java
@@ -0,0 +1,160 @@
+package doext.implement;
+
+import java.util.Map;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.widget.EditText;
+import core.helper.DoTextHelper;
+import core.helper.DoUIModuleHelper;
+import core.helper.jsonparse.DoJsonNode;
+import core.interfaces.DoIScriptEngine;
+import core.interfaces.DoIUIModuleView;
+import core.object.DoInvokeResult;
+import core.object.DoUIModule;
+import doext.define.do_TextBox_IMethod;
+import doext.define.do_TextBox_MAbstract;
+
+/**
+ * 自定义扩展UIView组件实现类，此类必须继承相应VIEW类，并实现DoIUIModuleView,Do_TextBox_IMethod接口；
+ * #如何调用组件自定义事件？可以通过如下方法触发事件：
+ * this.model.getEventCenter().fireEvent(_messageName, jsonResult);
+ * 参数解释：@_messageName字符串事件名称，@jsonResult传递事件参数对象； 获取DoInvokeResult对象方式new
+ * DoInvokeResult(this.model.getUniqueKey());
+ */
+public class do_TextBox_View extends EditText implements DoIUIModuleView, do_TextBox_IMethod {
+
+	/**
+	 * 每个UIview都会引用一个具体的model实例；
+	 */
+	private do_TextBox_MAbstract model;
+
+	public do_TextBox_View(Context context) {
+		super(context);
+		this.setSingleLine(false);
+		this.setPadding(1, 0, 1, 0);
+		this.setGravity(Gravity.TOP | Gravity.LEFT);
+		this.setLineSpacing(1.1f, 1.2f);
+	}
+
+	/**
+	 * 初始化加载view准备,_doUIModule是对应当前UIView的model实例
+	 */
+	@Override
+	public void loadView(DoUIModule _doUIModule) throws Exception {
+		this.model = (do_TextBox_MAbstract) _doUIModule;
+		this.setTextSize(TypedValue.COMPLEX_UNIT_PX, DoUIModuleHelper.getDeviceFontSize(_doUIModule, "9"));
+		this.addTextChangedListener(textWatcher);
+	}
+
+	/**
+	 * 动态修改属性值时会被调用，方法返回值为true表示赋值有效，并执行onPropertiesChanged，否则不进行赋值；
+	 * 
+	 * @_changedValues<key,value>属性集（key名称、value值）；
+	 */
+	@Override
+	public boolean onPropertiesChanging(Map<String, String> _changedValues) {
+		return true;
+	}
+
+	/**
+	 * 属性赋值成功后被调用，可以根据组件定义相关属性值修改UIView可视化操作；
+	 * 
+	 * @_changedValues<key,value>属性集（key名称、value值）；
+	 */
+	@Override
+	public void onPropertiesChanged(Map<String, String> _changedValues) {
+		DoUIModuleHelper.handleBasicViewProperChanged(this.model, _changedValues);
+		DoUIModuleHelper.setFontProperty(this.model, _changedValues);
+		if (_changedValues.containsKey("hint")) {
+			this.setHint(_changedValues.get("hint"));
+		}
+		if (_changedValues.containsKey("maxLength")) {
+			int imaxLength = DoTextHelper.strToInt(_changedValues.get("maxLength"), 0);
+			if (imaxLength > 0) {
+				this.setFilters(new InputFilter[] { new InputFilter.LengthFilter(imaxLength) });
+			}
+		}
+	}
+
+	/**
+	 * 同步方法，JS脚本调用该组件对象方法时会被调用，可以根据_methodName调用相应的接口实现方法；
+	 * 
+	 * @_methodName 方法名称
+	 * @_dictParas 参数（K,V）
+	 * @_scriptEngine 当前Page JS上下文环境对象
+	 * @_invokeResult 用于返回方法结果对象
+	 */
+	@Override
+	public boolean invokeSyncMethod(String _methodName, DoJsonNode _dictParas, DoIScriptEngine _scriptEngine, DoInvokeResult _invokeResult) throws Exception {
+		return false;
+	}
+
+	/**
+	 * 异步方法（通常都处理些耗时操作，避免UI线程阻塞），JS脚本调用该组件对象方法时会被调用， 可以根据_methodName调用相应的接口实现方法；
+	 * 
+	 * @_methodName 方法名称
+	 * @_dictParas 参数（K,V）
+	 * @_scriptEngine 当前page JS上下文环境
+	 * @_callbackFuncName 回调函数名 #如何执行异步方法回调？可以通过如下方法：
+	 *                    _scriptEngine.callback(_callbackFuncName,
+	 *                    _invokeResult);
+	 *                    参数解释：@_callbackFuncName回调函数名，@_invokeResult传递回调函数参数对象；
+	 *                    获取DoInvokeResult对象方式new DoInvokeResult(this.model.getUniqueKey());
+	 */
+	@Override
+	public boolean invokeAsyncMethod(String _methodName, DoJsonNode _dictParas, DoIScriptEngine _scriptEngine, String _callbackFuncName) {
+		return false;
+	}
+
+	/**
+	 * 释放资源处理，前端JS脚本调用closePage或执行removeui时会被调用；
+	 */
+	@Override
+	public void onDispose() {
+	}
+
+	private void doTextBoxView_TextChanged() {
+		if (this.model.getCurrentPage().getScriptEngine() != null) { // 去除脚本还未加载时NULL的情况
+			DoInvokeResult _invokeResult = new DoInvokeResult(this.model.getUniqueKey());
+			this.model.getEventCenter().fireEvent("textChanged", _invokeResult);
+		}
+	}
+
+	TextWatcher textWatcher = new TextWatcher() {
+		@Override
+		public void afterTextChanged(Editable arg0) {
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+		}
+
+		@Override
+		public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+			model.setPropertyValue("text", getText().toString());
+			doTextBoxView_TextChanged();
+		}
+	};
+
+	/**
+	 * 重绘组件，构造组件时由系统框架自动调用；
+	 * 或者由前端JS脚本调用组件onRedraw方法时被调用（注：通常是需要动态改变组件（X、Y、Width、Height）属性时手动调用）
+	 */
+	@Override
+	public void onRedraw() {
+		this.setLayoutParams(DoUIModuleHelper.getLayoutParams(this.model));
+	}
+
+	/**
+	 * 获取当前model实例
+	 */
+	@Override
+	public DoUIModule getModel() {
+		return model;
+	}
+}
